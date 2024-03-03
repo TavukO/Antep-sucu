0d6c8c2f4c75b485a7b6043eb23199fa44f52394
TavukO <162047381+TavukO@users.noreply.github.com>
2024-03-03 21:03:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("findOutBtn").addEventListener("click", function() {
+    var message = "Sen Dünyanın En İyi Kızısın! 🐱";
+    document.getElementById("message").textContent = message;
+  });
+  
